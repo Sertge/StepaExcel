733ee9aa180cfc309059c2f3c5ed387c955d1bc9
LAPTOP-5VLIPF13\Sertge <sergiojaime@ballastlane.com>
2024-10-02 23:12:57 -0500

    Include sheet handler

---
+++ StepaExcelReporter/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SpreadsheetLight;
+using StepaExcelReporter.Components;
+
+namespace StepaExcelReporter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private class InternalDataContext
+        {
+            public Uri? ExcelFilePath { get; set; }
+            public string XpsTempFIleName { get; set; }
+            public InternalDataContext() {
+                XpsTempFIleName = "tempXpsFile";
+            }
+        }
+        private readonly InternalDataContext _internalDataContext;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _internalDataContext = new InternalDataContext();
+            this.DataContext = new InternalDataContext();
+        }
+
+        private void mw_explore_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.DefaultExt = ".xlsx"; // Default file extension
+            dialog.Filter = "Excel Files (.xlsx)|*.xlsx";
+            //"Text documents (.txt)|*.txt"
+            bool? result = dialog.ShowDialog();
+
+            if (result == true) {
+                _internalDataContext.ExcelFilePath= new Uri(dialog.FileName);
+                string filename = dialog.FileName;
+                using (SLDocument Doc = new SLDocument(filename))
+                {
+                    bool sheetExists = Doc.SelectWorksheet("excel");
+
+                    if (sheetExists == true)
+                    {
+                        var selectedString = Doc.GetCellValueAsString("A2");
+                    }
+
+                }
+                //mw_docview.Path = dialog.FileName;
+            }
+        }
+    }
+}
---
+++ StepaExcelReporter/Models/SidePanelOption.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepaExcelReporter.Models
+{
+    public class SidePanelOption
+    {
+        public string Title {  get; set; }
+        public string Icon { get; set; }
+        public string Path { get; set; }
+        SidePanelOption() { }
+    }
+    public class OptionsCollection : ObservableCollection<SidePanelOption> { }
+}
